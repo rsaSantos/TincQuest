4480f9347dccf350c8c6444b60c5a78a4b130a73
LCMJ21 <jorge.lima.ne@gmail.com>
2023-04-01 01:32:33 +0100

    gitignore added

--- Backend/database/models.py
+++ Backend/database/models.py
@@ -8,6 +8,7 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
