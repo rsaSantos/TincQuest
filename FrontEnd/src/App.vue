a3a7cb8adfa5ca8d47de134fddd90850b6ec00af
sucodlaranja <joaoandre210@gmail.com>
2023-04-01 10:34:33 +0100

    login and events

--- FrontEnd/src/App.vue
+++ FrontEnd/src/App.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router'
+import { RouterView, useRoute } from 'vue-router'
 import TopBar from './components/TopBar.vue'
+import { UNPROTECTED_ROUTES } from './router/router'
+import { computed } from 'vue'
+
+const notInProtectedRoutes = computed(() => {
+  const route = useRoute().name
+  if (!route) return false
+  return !UNPROTECTED_ROUTES.includes(route.toString())
+})
 </script>
 
 <template>
-  <div class="bg-white pt-2 h-screen w-screen">
-    <TopBar />
+  <div class="bg-white pt-2 h-screen w-full">
+    <TopBar v-if="notInProtectedRoutes" />
     <RouterView />
   </div>
 </template>
---
+++ FrontEnd/src/components/EventQuestions.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div>Questions</div>
+</template>
